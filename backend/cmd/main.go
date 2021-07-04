beb5df39199c239419c28b7f1c963aba5546fb3a
DimKush <dimkysh@gmail.com>
2021-07-04 21:12:47 +0300

    02-init | first iteration of logger with zerolog help

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
+import (
+	"github.com/DimKush/guestbook/tree/main/backend/internal/Logger"
+)
+
 func main() {
 	//Logger.Instance().Write(Logger.ERROR, "Logger init t")
 	//Logger.Instance().Write(Logger.DEBUG, "Logger init DEBUG")
-	Logger.
-		log.Print("print")
+	Logger.Instance().Log().Error().Msgf("Logger init t %d", 10)
+	Logger.Instance().Log().Debug().Msgf("Logger init t %d", 10)
+	Logger.Instance().Log().Info().Msgf("Logger init t %d", 10)
+
 }
