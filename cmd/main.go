1b7a43aad2a64a75d1b4182b4503e3ba14ac73a3
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-07-22 21:57:02 +0300

    07-clean | complete panic with error

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,6 @@ func main() {
 	//TODO : from config
 	strPort := "8040"
 	if err := server.Run(strPort); err != nil {
-		panic(fmt.Sprintf("Cannot run server on port : %s", strPort))
+		panic(fmt.Sprintf("Cannot run server on port : %s. Reason : %s", strPort, err.Error()))
 	}
 }
