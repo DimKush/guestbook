f10262202f5ea7bc5b3bf5c45f32d41774293e68
DimKush <demonmadman228@gmail.com>
2021-07-22 20:17:48 +0300

    07-clean | Matt Rayer hack in main function

--- cmd/main.go
+++ cmd/main.go
@@ -8,8 +8,14 @@ import (
 
 func main() {
 	server := new(server.Server)
-
+	if err := run(server); err != nil {
+		panic(err.Error())
+	}
 	//TODO : from config
+
+}
+
+func run(server *server.Server) error {
 	strPort := "8040"
 	if err := server.Run(strPort); err != nil {
 		panic(fmt.Sprintf("Cannot run server on port : %s. Reason : %s", strPort, err.Error()))
