90f6660371287b802b62da58fbac9b820280045f
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-07-23 20:49:02 +0300

    07-clean | golang way config

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"strings"
 
 	server "github.com/DimKush/guestbook/tree/main"
 	"github.com/DimKush/guestbook/tree/main/pkg/handler"
+	"github.com/spf13/viper"
 )
 
 func main() {
@@ -22,9 +25,34 @@ func run(server *server.Server) error {
 
 	routes := handler.InitRoutes()
 
+	// read config
+	if err := InitConfig(); err != nil {
+		return fmt.Errorf("Cannot read service config. Reason: %s", err.Error())
+	}
+
 	if err := server.Run(strPort, routes); err != nil {
 		return fmt.Errorf("Cannot run server on port : %s. Reason : %s", strPort, err.Error())
 	}
 
 	return nil
+}
+
+func InitConfig() error {
+	platform := strings.ToLower(runtime.GOOS)
+
+	var confDirPath string
+	switch platform {
+	case "linux":
+		{
+			confDirPath = "/opt/dimkush_guestbook/conf"
+		}
+	case "windows":
+		{
+			confDirPath = "c:\\dimkush_guestbook\\conf"
+		}
+	}
+
+	viper.AddConfigPath(confDirPath)
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ pkg/handler/lists.go
@@ -0,0 +1,25 @@
+package handler
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) createList(context *gin.Context) {
+
+}
+
+func (h *Handler) getAllLists(context *gin.Context) {
+
+}
+
+func (h *Handler) getListById(context *gin.Context) {
+
+}
+
+func (h *Handler) updateListById(context *gin.Context) {
+
+}
+
+func (h *Handler) dropListById(context *gin.Context) {
+
+}
