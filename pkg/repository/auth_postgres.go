58c8fbc1b077918c33252faaeb6be28c65351a7f
Дмитрий Кушнарюк <dkushnaryuk@diasoft.ru>
2021-07-27 01:14:34 +0300

    08-auth | check if username exists

--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -1,6 +1,8 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/DimKush/guestbook/tree/main/internal/entities/User"
 	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
@@ -15,6 +17,12 @@ func InitAuthPostgres(database *gorm.DB) *AuthPostgres {
 }
 
 func (data *AuthPostgres) CreateUser(user User.User) (int, error) {
+	var count int64
+	data.db.Table(users).Where("username=?", user.Username).Count(&count)
+	if count != 0 {
+		return 0, fmt.Errorf("Username with username : %s already exists.", user.Username)
+	}
+
 	err := data.db.Table(users).Create(&user).Error
 
 	if err != nil {
