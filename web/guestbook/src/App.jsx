ae110b8d683ea8d20132167e289dfe512dad6530
DimKush <dimkysh@gmail.com>
2021-07-30 02:27:31 +0300

    11-web-auth | sign-in and sign-up form

--- web/guestbook/src/App.jsx
+++ web/guestbook/src/App.jsx
@@ -1,10 +1,53 @@
 import logo from './logo.svg';
-import './App.css';
-import { SignIn } from "./components/login/index"
-function App() {
+import React from 'react';
+import './App.scss';
+import { SignIn, SignUp } from "./components/login/index"
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isLoggingActive: true
+    }
+  }  
+
+  changeState() {
+    const { isLoggingActive } = this.state;
+    if(isLoggingActive) {
+      this.LeftSightComponent.classList.remove("right");
+      this.LeftSightComponent.classList.add("left");
+    } else {
+      this.LeftSightComponent.classList.remove("left");
+      this.LeftSightComponent.classList.add("right");
+    }
+
+    this.setState((prevState) => ({ isLoggingActive : !prevState.isLoggingActive }))
+  }
+
+  render(){
+    const {isLoggingActive} = this.state;
+    let currentState = isLoggingActive ? "Sign up" : "Sign in";
+    //const  currentActive = isLoggingActive ? "Sign in" : "Sign up";
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container">
+            {isLoggingActive && <SignIn containerRef={(ref) => this.current = ref} />}
+            {!isLoggingActive && <SignUp containerRef={(ref) => this.current = ref} />}
+          </div>
+          <RightSightComponent current={currentState} containerRef={ref => this.LeftSightComponent = ref} onClick={this.changeState.bind(this)}/>
+        </div>
+      </div>
+    );
+  }
+}
+
+const RightSightComponent = props => {
   return (
-    <div className="App">
-      <SignIn />
+    <div className="right-side" ref={props.containerRef} onClick={props.onClick}>
+      <div className="inner-container">
+         <div className="text">{props.current}</div>
+      </div>
     </div>
   );
 }
--- web/guestbook/src/components/login/sign-in.jsx
+++ web/guestbook/src/components/login/sign-in.jsx
@@ -8,7 +8,7 @@ export class SignIn extends React.Component{
 
 	render(){
 		return (
-			<div className="base-container">
+			<div className="base-container" ref={this.props.containerRef}>
 				<div className="header">SIGN IN TO DK-GUESTBOOK</div>
 				<div className="content">
 				<div className="image"> 
@@ -16,16 +16,14 @@ export class SignIn extends React.Component{
 				</div>
 					<div className="form">
 						<div className="form-group">
-							<label htmlFor="username">Username</label>
 							<input type="text" name="username" placeholder="Username"/>
 						</div>
 						<div className="form-group">
-							<label htmlFor="pasword">Password</label>
 							<input type="password" name="password" placeholder="Password"/>
 						</div>
 					</div>
 					<div className="footer">
-						<button type="button" className="btn-signIn">SIGN IN</button>
+						<button type="button" className="btn">SIGN IN</button>
 					</div>
 				</div>
 			</div>
