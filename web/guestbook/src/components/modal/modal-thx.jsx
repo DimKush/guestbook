112b11a1e3abcb63921530299e34c331bb3e58e1
DimKush <dimkysh@gmail.com>
2021-08-16 21:48:44 +0300

    13-web-lists | new modal window thx for registering

---
+++ web/guestbook/src/components/modal/modal-thx.jsx
@@ -0,0 +1,33 @@
+import React, { Children } from 'react';
+import "./modal_style.scss"
+
+
+const ModalThx = ({active, setActive}) => {
+	const handleOkClick = () => {
+		setActive(false);
+	}
+
+	
+	return (
+		<div className={active ? "modal active": "modal "} onClick={() => setActive(false)}>
+			<div className={active ? "modal_thx_content active" : "modal_thx_content"} onClick={e => e.stopPropagation()}>
+				<div className="header-part">
+					<h1>THANKS FOR REGISTERING</h1>
+				</div>
+				<div className="textDescr-part">
+					<p>Golang/React service template text. Golang/React service template text. Golang/React service template text. Golang/React service template text.</p>
+					<p>Golang/React service template text. Golang/React service template text. Golang/React service template text. Golang/React service template text.</p>
+					<p>Golang/React service template text. Golang/React service template text. Golang/React service template text. Golang/React service template text.</p>
+					<p>Golang/React service template text. Golang/React service template text. Golang/React service template text. Golang/React service template text.</p>
+				</div>
+				
+				<div className="modal_footer">
+					<button className="btn" onClick={handleOkClick}>OK</button>
+				</div>
+			</div>
+			
+		</div>
+	);
+};
+
+export default ModalThx;
