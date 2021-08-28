749db3cbae32eb9aff8a7086f9db2ef3d1076f2d
DimKush <dimkysh@gmail.com>
2021-08-28 17:28:27 +0300

    15-web-delete-list | async calling of current user in delete-list.js

--- web/guestbook/src/components/home-list-delete/delete-list.jsx
+++ web/guestbook/src/components/home-list-delete/delete-list.jsx
@@ -3,8 +3,32 @@ import { cookies } from "../../App";
 import Modal from "../modal/modal.jsx";
 
 
-export default function DeleteList(selectedRow, currentUser) {
+async function getUsername() {
+	try {
+	const responce = await fetch("http://localhost:8007/auth/user", {
+			  headers : { "Content-type" : "application/json",
+						"Authorization" :`Bearer ${cookies.get("jwt")}`},
+			  credentials : "include",
+			});
+
+		const content = await responce.json();
+
+		if(content.Status === "OK"){
+			return content.username;
+		} else {
+			// TODO: Modal error
+		}
+	} catch(error) {
+
+	}
+}
+
+export default async function DeleteList(selectedRow) {
 	let result = {Status : "", Message : ""};
+
+	const currentUser = await getUsername();
+	
+	console.log("currentUser", currentUser);
 
 	if(currentUser !== selectedRow.owner){
 		result.Status = "Error";
@@ -12,11 +36,8 @@ export default function DeleteList(selectedRow, currentUser) {
 
 		return result;
 	}
-	
-	(async () => {
-		
-	})();
+	result.Status="OK";
+	result.Message="Record deleted";
 
-
-	return "OK";
+	return result;
 }
