6d821fbef2f22937a3e2faa5976e2ce4294b093f
DimKush <dimkysh@gmail.com>
2021-08-30 20:54:07 +0300

    17-web-events-list | events list prepare repository

--- internal/entities/EventItem/EventItem.go
+++ internal/entities/EventItem/EventItem.go
@@ -4,7 +4,7 @@ type EventItem struct {
 	Id            int    `json:"id" gorm:"id"`
 	ListId        int    `json:"-" gorm:"list_id"`
 	ListTile      string `json:"list_title" gorm:"title"`
-	EventTypeName string `json:"event_type" gorm:"systemname"`
+	EventTypeName string `json:"event_type" gorm:"fullname"`
 	EventTypeId   int    `json:"-" gorm:"type_id"`
 	Description   string `json:"description" gorm:"description"`
 }
